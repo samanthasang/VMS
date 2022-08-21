c69a2c0b3e20277d1d755c80b3dd5148e97103cf
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-08-21 12:44:36 +0430

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 git pull origin main
+sudo docker rm -f vms-front
 sudo docker build -t vms-front .
-sudo docker run -d --restart=always -p 3000:3000 -it vms-front
+sudo docker run -d --name vms-front --restart=always -p 3000:3000 -it vms-front
